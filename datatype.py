1c3cd750a8a169552b5c1bdb42bad70d74bfd8a5
Raúl <raul.hernandez@testeandosoftware.com>
2019-11-11 13:02:56 +0100

    Added datatype.py

---
+++ datatype.py
@@ -0,0 +1,48 @@
+# Strings
+print("Hello World")
+print('Hello World')
+print("""Hello World""")
+print('''Hello World''')
+print(type("Hello World"))
+print("Bye" + " World")
+
+#Integer
+print(30)
+print(type(30))
+
+#Float
+print(30.5)
+print(type(30.5))
+
+#Boolean
+True
+print(type(True))
+False
+print(type(False))
+
+#List
+[10, 20, 30, 55]
+print(type([10, 20, 30, 55]))
+["Hola", "Adios"]
+print(type(["Hola", "Adios"]))
+
+#Tuple
+(10, 20, 30, 55)
+print(type((10, 20, 30, 55)))
+
+#Dictionary
+{
+    "name": "Ryan",
+    "lastname": "Ray",
+    "nickname": "Fazt"
+}
+
+print(type({
+    "name": "Ryan",
+    "lastname": "Ray",
+    "nickname": "Fazt"
+}))
+
+# None
+None
+print(type(None))
